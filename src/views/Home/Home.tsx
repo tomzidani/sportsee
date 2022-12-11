aca441c5f70fdb93873df4dee6b7f8bfeb7bdd34
Tom Zidani <tom.zidani@gmail.com>
2022-12-11 16:01:39 +0100

    Add home user selection.

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
     <main className="app-home">
       <div className="home__wrapper">
-        <h2>Hello World !</h2>
+        <section className="home__header">
+          <h1 className="home__title">SportSee</h1>
+          <p className="home__text">La plateforme de coaching sportif en ligne</p>
+        </section>
+        <section className="home__users">
+          <ul>
+            <li>
+              <Link to="/profil/12">User 12</Link>
+            </li>
+            <li>
+              <Link to="/profil/18">User 18</Link>
+            </li>
+          </ul>
+        </section>
       </div>
     </main>
   )
